5dda9a8787197793e29c01c25d59b04314869a39
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2023-09-09 13:43:23 +0200

    Add ability to read input file from command line

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,37 @@
 import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import tokenizer.*;
 import parser.*;
 
 public class Main {
   public static void main (String args[]) {
-    String input = "exists x, y Brother(x, Richard) + Brother(y, Richard) + -(x = y))";
-    System.out.println (input);
-    testParse (input);
+    if (args.length < 1) {
+      System.out.println(args[0]);
+      return;
+    }
+
+    Scanner sc;
+    File file = new File(args[0]);
+    try {
+      sc = new Scanner(file);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return;
+    }
+
+    while (sc.hasNextLine()) {
+      testScan(sc.nextLine());
+    }
   }
 
-  private static void testParse (String input) {
+  private static void testScan (String input) {
     Tokenizer tokens = new Tokenizer(input);
+    System.out.println(input);
     tokens.print();
-    if (Parser.parse(tokens)) {
+    if (!tokens.containsErrors()) {
       System.out.println ("Success");
     } else {
       System.out.println ("Failure");
